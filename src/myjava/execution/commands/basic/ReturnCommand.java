ae43bbea302fa494ea83f70d6ec4ba0c5a621d38
HazyNick <ang.nik10@gmail.com>
2017-11-17 19:29:00 +0800

    Remaining basic stuff-

---
+++ src/myjava/execution/commands/basic/ReturnCommand.java
@@ -0,0 +1,51 @@
+package myjava.execution.commands.basic;
+
+import myjava.builder.errorcheckers.TypeChecker; 
+import myjava.builder.errorcheckers.UndeclaredChecker; 
+import myjava.execution.ICommand; 
+import myjava.execution.commands.evaluate.EvaluationCommand; 
+import myjava.generatedexp.JavaParser.ExpressionContext; 
+import myjava.ide.console.Console; 
+import myjava.ide.console.LogItemView.LogType; 
+import myjava.semantics.representations.MyJAVAFunction; 
+import myjava.semantics.representations.MyJAVAValue; 
+import myjava.semantics.utils.AssignmentUtils;
+
+/**
+ * Represents a return command which is specially used by a function.
+ * @author NeilDG
+ *
+ */
+public class ReturnCommand implements ICommand {
+    private final static String TAG = "MyJAVAProg_ReturnCommand";
+
+    private ExpressionContext expressionCtx;
+    private MyJAVAFunction assignedMyJAVAFunction;
+
+    public ReturnCommand(ExpressionContext expressionCtx, MyJAVAFunction myJAVAFunction) {
+        this.expressionCtx = expressionCtx;
+        this.assignedMyJAVAFunction = myJAVAFunction;
+
+        UndeclaredChecker undeclaredChecker = new UndeclaredChecker(this.expressionCtx);
+        undeclaredChecker.verify();
+
+        MyJAVAValue myJAVAValue = this.assignedMyJAVAFunction.getReturnValue();
+        TypeChecker typeChecker = new TypeChecker(myJAVAValue, this.expressionCtx);
+        typeChecker.verify();
+    }
+
+    /* (non-Javadoc)
+     * @see myjava.execution.commands.ICommand#execute()
+     */
+    @Override
+    public void execute() {
+        EvaluationCommand evaluationCommand = new EvaluationCommand(this.expressionCtx);
+        evaluationCommand.execute();
+
+        MyJAVAValue myJAVAValue = this.assignedMyJAVAFunction.getReturnValue();
+
+        AssignmentUtils.assignAppropriateValue(myJAVAValue, evaluationCommand.getResult());
+        //Console.log(LogType.DEBUG,"Return value is: " +evaluationCommand.getResult().toEngineeringString());
+    }
+
+}
