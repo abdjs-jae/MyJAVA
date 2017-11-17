d164d6e78aa37b7bf2c9bbae5cbe839330f1175b
HazyNick <ang.nik10@gmail.com>
2017-11-17 22:42:40 +0800

    Updated info

--- src/myjava/execution/commands/basic/IntDecCommand.java
+++ src/myjava/execution/commands/basic/IntDecCommand.java
@@ -1,4 +1,126 @@
 package myjava.execution.commands.basic;
 
-public class IntDecCommand {
+import myjava.MyJAVALexer;
+import myjava.builder.ParserHandler; 
+import myjava.execution.ICommand;
+import myjava.generatedexp.JavaParser.ExpressionContext; 
+import myjava.semantics.mapping.ClassIdentifierMapper; 
+import myjava.semantics.mapping.IValueMapper; 
+import myjava.semantics.mapping.IdentifierMapper; 
+import myjava.semantics.representations.MyJAVAValue; 
+import myjava.semantics.representations.MyJAVAValueSearcher;
+import myjava.semantics.representations.MyJAVAValue.PrimitiveType; 
+import myjava.semantics.symboltable.SymbolTableManager; 
+import myjava.semantics.symboltable.scopes.ClassScope;
+
+/**
+ * An increment or decrement command
+ * @author NeilDG
+ *
+ */
+public class IntDecCommand implements ICommand {
+
+    private ExpressionContext exprCtx;
+    private int tokenSign;
+
+    public IntDecCommand(ExpressionContext exprCtx, int tokenSign) {
+        this.exprCtx = exprCtx;
+        this.tokenSign = tokenSign;
+    }
+
+    /* (non-Javadoc)
+     * @see myjava.execution.commands.ICommand#execute()
+     */
+    @Override
+    public void execute() {
+        //String identifier = this.exprCtx.primary().Identifier().getText();
+        //MyJAVAValue myJAVAValue = MyJAVAValueSearcher.searchMyJAVAValue(identifier);
+
+        IValueMapper leftHandMapper = new IdentifierMapper(
+                this.exprCtx.getText());
+        leftHandMapper.analyze(this.exprCtx);
+
+        MyJAVAValue myJAVAValue = leftHandMapper.getMyJAVAValue();
+
+        this.performOperation(myJAVAValue);
+    }
+
+    /*
+     * Attempts to perform an increment/decrement operation
+     */
+    private void performOperation(MyJAVAValue myJAVAValue) {
+        if(myJAVAValue.getPrimitiveType() == PrimitiveType.INT) {
+            int value = Integer.valueOf(myJAVAValue.getValue().toString());
+
+            if(this.tokenSign == MyJAVALexer.INC) {
+                value++;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+            else if(this.tokenSign == MyJAVALexer.DEC) {
+                value--;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+        }
+        else if(myJAVAValue.getPrimitiveType() == PrimitiveType.LONG) {
+            long value = Long.valueOf(myJAVAValue.getValue().toString());
+
+            if(this.tokenSign == MyJAVALexer.INC) {
+                value++;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+            else if(this.tokenSign == MyJAVALexer.DEC) {
+                value--;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+        }
+        else if(myJAVAValue.getPrimitiveType() == PrimitiveType.BYTE) {
+            byte value = Byte.valueOf(myJAVAValue.getValue().toString());
+
+            if(this.tokenSign == MyJAVALexer.INC) {
+                value++;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+            else if(this.tokenSign == MyJAVALexer.DEC) {
+                value--;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+        }
+        else if(myJAVAValue.getPrimitiveType() == PrimitiveType.SHORT) {
+            short value = Short.valueOf(myJAVAValue.getValue().toString());
+
+            if(this.tokenSign == MyJAVALexer.INC) {
+                value++;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+            else if(this.tokenSign == MyJAVALexer.DEC) {
+                value--;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+        }
+        else if(myJAVAValue.getPrimitiveType() == PrimitiveType.FLOAT) {
+            float value = Float.valueOf(myJAVAValue.getValue().toString());
+
+            if(this.tokenSign == MyJAVALexer.INC) {
+                value++;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+            else if(this.tokenSign == MyJAVALexer.DEC) {
+                value--;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+        }
+        else if(myJAVAValue.getPrimitiveType() == PrimitiveType.DOUBLE) {
+            double value = Double.valueOf(myJAVAValue.getValue().toString());
+
+            if(this.tokenSign == MyJAVALexer.INC) {
+                value++;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+            else if(this.tokenSign == MyJAVALexer.DEC) {
+                value--;
+                myJAVAValue.setValue(String.valueOf(value));
+            }
+        }
+    }
+
 }
